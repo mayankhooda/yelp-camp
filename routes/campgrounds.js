20a2148f1d20eac542910004bf406c42982caf22
mayankhooda <mayankhooda@yahoo.com>
2017-12-23 08:20:06 +0530

    add campground user association

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -19,8 +19,11 @@ router.post("/", function(req, res) {
     var name = req.body.name;
     var image = req.body.image;
     var description = req.body.description;
-
-    var newCampground = { name: name, image: image, description: description };
+    var author = {
+        id: req.user._id,
+        username: req.user.username
+    };
+    var newCampground = { name: name, image: image, description: description, author: author};
     Campground.create(newCampground, function(err, campground) {
         if (err) {
             console.log(err);
@@ -31,7 +34,7 @@ router.post("/", function(req, res) {
     });
 });
 
-router.get("/new", function(req, res) {
+router.get("/new", isLoggedIn, function(req, res) {
     res.render("campgrounds/new.ejs");
 });
 
@@ -48,5 +51,12 @@ router.get("/:show", function(req, res) {
             }
         });
 });
+
+function isLoggedIn(req, res, next) {
+    if (req.isAuthenticated()) {
+        return next();
+    }
+    res.redirect("/login");
+}
 
 module.exports = router;
